b05c5ce493d94c85d1569994ad4bee9ff98c5324
Krishna Sagiraju <krishna@bluedata.com>
2018-07-19 19:37:31 -0400

    Use a diff tgz url

--- BD-Centos/init.sh
+++ BD-Centos/init.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-yum install -y epel-release
-yum install -y python-setuptools python-requests python-argparse docker wget
+yum install -d0 -e1 -y epel-release
+yum install -d0 -e1 -y python-setuptools python-requests python-argparse docker wget
 
 systemctl start docker
 
 cd /
-wget https://github.com/SahithiGunna/katacoda-scenarios/blob/master/BD-Centos/catalogsdk-master.tgz
+wget https://github.com/SahithiGunna/katacoda-scenarios/raw/master/BD-Centos/catalogsdk-master.tgz
 tar -xzf /catalogsdk-master.tgz
 cd /catalogsdk-master
 python setup.py develop
