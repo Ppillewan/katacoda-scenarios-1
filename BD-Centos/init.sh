082260bae592ab674354d77b1efbddb005b89c68
Krishna Sagiraju <krishna@bluedata.com>
2018-07-19 15:00:43 -0400

    Another attempt at uploading sdk tgz.

--- BD-Centos/init.sh
+++ BD-Centos/init.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+set -e
+
 yum install -y epel-release
 yum install -y python-setuptools python-requests python-argparse docker
 
 systemctl start docker
 
-tar xzf -C / /catalogsdk-master.tgz
+tar -C / -xzf /catalogsdk-master.tgz
 cd /catalogsdk-master
 python setup.py develop
