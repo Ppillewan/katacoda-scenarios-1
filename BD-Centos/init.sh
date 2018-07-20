ad18986355b1483b3bdd9449e9b38f8e9ad78f29
Krishna Sagiraju <krishna@bluedata.com>
2018-07-20 14:32:39 -0400

    Still trying to get it working.

--- BD-Centos/init.sh
+++ BD-Centos/init.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
+set -e
+
 yum install -d0 -e1 -y epel-release
-yum install -d0 -e1 -y python-setuptools python-requests python-argparse docker wget
+yum install -d2 -e1 -y python-setuptools python-requests python-argparse docker wget
 
 systemctl start docker
 
 cd /
 wget https://github.com/SahithiGunna/katacoda-scenarios/raw/master/BD-Centos/catalogsdk-master.tgz
-tar -xzf /catalogsdk-master.tgz
+tar xzf /catalogsdk-master.tgz
 cd /catalogsdk-master
 python setup.py develop
 
+clear
 exit 0
