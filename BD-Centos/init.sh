a0c04f200413777f65107b0ea281ed10234068f8
Krishna Sagiraju <krishna@bluedata.com>
2018-07-23 10:18:23 -0400

    Put init.sh back with minimal code for tutorial1

--- BD-Centos/init.sh
+++ BD-Centos/init.sh
@@ -1,18 +1,3 @@
 #!/usr/bin/env bash
-
-yum install -d0 -e1 -y epel-release
-yum install -d2 -e1 -y python-pip python-setuptools python-requests python-argparse docker wget
-
-pip install --upgrade argparse
-
-systemctl start docker
-
-cd /
-wget https://github.com/SahithiGunna/katacoda-scenarios/raw/master/BD-Centos/catalogsdk-master.tgz
-tar xzf /catalogsdk-master.tgz
-cd /catalogsdk-master
-python setup.py develop
-
-mkdir -p ~/tutorials
-cd ~/tutorials
-clear
+mkdir -p ~/tutorial1
+cd ~/tutorial1
